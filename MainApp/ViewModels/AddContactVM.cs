f2e9cd7a85afbef411ca5aaab7e23e573bea15e4
Rasim <allen.wallker.00@bk.ru>
2021-01-12 19:02:45 +0400

    Validation has been added

--- MainApp/ViewModels/AddContactVM.cs
+++ MainApp/ViewModels/AddContactVM.cs
@@ -4,33 +4,45 @@ using MainApp.Models;
 using MainApp.Services;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace MainApp.ViewModels
 {
-    class AddContactVM : ViewModelBase
+    class AddContactVM : ViewModelBase, IDataErrorInfo
     {
         private RelayCommand addButtonCommand;
 
         IStorage storage;
 
+        public bool IsValid { get; set; } = true;
+
         public static int Count = 100;
 
+        [Required]
         public string FirstNameProp { get; set; }
 
+        [Required]
         public string LastNameProp { get; set; }
 
+        [Required]
+        [Phone]
         public string PhoneNumberProp { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string EmailAdressProp { get; set; }
 
         public string PhotoProp { get; set; }
 
         public bool FavoriteProp { get; set; }
 
+        [Required]
         public string JobProp { get; set; }
 
-        public RelayCommand AddButtonCommand => addButtonCommand ?? (addButtonCommand = new RelayCommand(() => Add()));
+        public RelayCommand AddButtonCommand => addButtonCommand ??= new RelayCommand(() => Add(), IsValid);
 
         public AddContactVM(IStorage storage)
         {
@@ -50,6 +62,27 @@ namespace MainApp.ViewModels
                 Favorite = FavoriteProp
             }) ;
             App.Container.GetInstance<ContactDetailsVM>().IncreaseCount();
+        }
+
+        public string Error => throw new NotImplementedException();
+
+        public string this[string columnName]
+        {
+            get
+            {
+                var context = new ValidationContext(this);
+                var results = new List<ValidationResult>();
+                 IsValid = Validator.TryValidateObject(this, context, results, true);
+                if (IsValid)
+                    return string.Empty;
+
+                var result = results.FirstOrDefault(x => x.MemberNames.Contains(columnName));
+
+                if (result is null)
+                    return string.Empty;
+
+                return result.ErrorMessage;
+            }
         }
     }
 }
