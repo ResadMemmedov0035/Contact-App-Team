b7d6a1404c141597095a0745caba9b15462e9dcb
ResadMemmedov0035 <77126746+ResadMemmedov0035@users.noreply.github.com>
2021-01-14 00:03:03 +0400

    Search Added

--- MainApp/ViewModels/HomePageVM.cs
+++ MainApp/ViewModels/HomePageVM.cs
@@ -1,22 +1,52 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using MainApp.Models;
 using MainApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
+using System.Windows;
 
 namespace MainApp.ViewModels
 {
     class HomePageVM : ViewModelBase
     {
         private IStorage Storage;
+        private IEnumerable<Contact> contacts;
+        private RelayCommand searchCommand;
+        private string search;
 
-        public ObservableCollection<Contact> Contacts => Storage.GetAll();
+        public IEnumerable<Contact> Contacts { get => contacts; set => Set(ref contacts, value); } /*=> Storage.GetAll();*/
+        public string Search
+        { get => search;
+            set
+            {
+                search = value;
+                SearchCommand.RaiseCanExecuteChanged();
+            }
+        }
+        
+        public RelayCommand SearchCommand => searchCommand ??= new RelayCommand(() =>
+        {
+            Contacts = Storage.GetAll();
+            Contacts = Contacts.Where(x =>
+            {
+                return
+                x.FirstName.Contains(Search) ||
+                x.LastName.Contains(Search) ||
+                x.PhoneNumber.Contains(Search) ||
+                x.Job.Contains(Search) ||
+                x.EmailAdress.Contains(Search);
+            });
+        },
+        () => !string.IsNullOrWhiteSpace(Search));
 
         public HomePageVM(IStorage storage)
         {
-            Storage = storage;   
+            Storage = storage;
+            Contacts = Storage.GetAll();
         }
     }
 }
